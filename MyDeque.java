5cbbb17e57c39bfd3cc708ddb1c75eba698e2ed0
Elizabeth Sham <esham00@stuy.edu>
2019-03-22 11:49:43 -0400

    LINE 79 CHECK IT

--- MyDeque.java
+++ MyDeque.java
@@ -43,21 +43,85 @@ public class MyDeque<E>{
 	return data[end];
     }
     public E removeFirst() {
+	E old = data[start];
+	data[start ] = null;
 	if (start == data.length-1) {
 	    start = 0;
+	    size--;
 	}
 	else {
 	    start++;
+	    size--;
 	}
-	return data[start-1];
+	return old;
     }
     public E removeLast() {
-	end--;
-	return data[end+1];
+	E old = data[end];
+	data[end] = null;
+	if(end == 0) {
+	    end = data.length-1;
+	    size--;
+	} else {
+	    end--;
+	    size--;
+	}
+	return old;
+    }
+    public E addFirst(E element) {
+	int old = data[start];
+	if(start < end) {
+	    data[start-1] = element;
+	    start--;
+	} else if (start == 0 && data[data.length-1] == null) {
+	    data[data.length-1] = element;
+	    start = data.length-1;
+	} else if (start > end) {
+		data[start-1] = element;
+		start--;
+	    }
+	}
+	else {
+	    resize();
+	    data[data.length-1] = element;
+	    start = data.length-1;
+	}
+	size++;
+	return old;
     }
     public E addLast(E element) {
+	E old = data[end];
 	if (end < data.length-1) {
-	    data[end+1] = element;
+	    if(data[end+1] == null) {
+		data[end+1] = element;
+		end++;
+	    }
+	} else if (data[0] == null)  {
+	    data[0] = element;
+	    end = 0;
 	} else {
-	    if (end 
-    public
+	    resize();
+	    data[end+1] = element;
+	    end++;
+	}
+	return old;
+    }
+    private void resize() {
+	E[] temp = new E[data.length * data.length];
+	if (start < end) {
+	    for(int i = 0; i < end-start; i++) {
+		temp[i] = data[i+start];
+	    }
+	} else {
+	    for(int i = 0; i < data.length-start; i++) {
+		if(data[i] != null) {
+		    temp[i] = data[i+start];
+		}
+	    }
+	    for(int i = 0; i < end; i++) {
+		temp[i] = data[i];
+	    }
+	}
+	start = 0;
+	end = size-1;
+	data = temp;
+    }
