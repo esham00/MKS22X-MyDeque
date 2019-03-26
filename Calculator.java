8b29844f3beb89d5ca1b8c0a57bc45cc99599020
Elizabeth Sham <esham00@stuy.edu>
2019-03-26 11:50:04 -0400

    almost there

--- Calculator.java
+++ Calculator.java
@@ -1,20 +1,27 @@
+import java.util.*;
 public class Calculator{
     public static double eval(String s){
-        MyDeque<Integer> values = new MyDeque<Integer>();
-	for(int i = 0; i < s.length(); i++) {
+        MyDeque<Double> values = new MyDeque<Double>();
+	String[] st = s.split(" ");
+	int j = 0;
+	for(int i = 0; i < st.length; i++) {
 	    try {
-	        if(i % 2 == 1) {
-		    values.addLast(Integer.parseInt(s));
-		} else {
-		    values.addFirst(Integer.parseInt(s));
+	        if(j % 2 == 1) {
+		    System.out.println(Double.parseDouble(st[i]));	    
+		    values.addLast(Double.parseDouble(st[i]));
+		} else {	
+		    System.out.println(Double.parseDouble(st[i]));	    
+		    values.addFirst(Double.parseDouble(st[i]));
 		}
+		j++;
 	    }
 	    catch(NumberFormatException e) {
-		int a = values.getFirst();
+		Double a = values.getFirst();
 		values.removeFirst();
-		int b = values.getLast();
+		Double b = values.getLast();
 		values.removeLast();
-		char operation = s.charAt(i);
+		char operation = st[i].charAt(0);
+		System.out.println(a + " " + operation +" "  + b);
 		if (operation == '+') {
 		    values.addLast(a+b);
 		} else if(operation == '*') {
@@ -24,7 +31,16 @@ public class Calculator{
 		} else if (operation == '-') {
 		    values.addLast(a-b);
 		}
+		System.out.println(values.getLast());
+		j = 0;
 	    }
 	}
+	return values.getFirst();
+    }
+    public static void main(String[] args) {
+	String a = "10 2.0 +";
+	String b = "11 3 - 4 + 2.5 *";
+	String c = "8 2 + 99 9 - * 2 + 9 -";
+	System.out.println(Calculator.eval(c));
     }
 }
